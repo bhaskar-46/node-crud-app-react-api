6730da6e9531fe56f6e0ad2dcae5dfb2eaea9e7c
bhaskar-46 <appasanibhaskar1@gmail.com>
2024-05-08 12:55:11 +0530

    first commit

---
+++ server/app/models/user.model.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("user", {
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      mobile: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  
+    return User;
+  };
+  
